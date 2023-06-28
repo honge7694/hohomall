c584af71f4f8ac00518af7ed661401d1496a85ba
hongui <honge7694@naver.com>
2023-06-28 14:12:55 +0900

    frontend init setting

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import Billing from "./pages/Billing";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+import "./index.scss";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
+
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+          <Route path="/sign-up" element={<SignUp/> }/>
+          <Route path="/sign-in" element={<SignIn/> }/>
+          <Route element={<Main />}>
+              <Route path="/dashboard" element={<Home/> }/>
+              <Route path="/tables" element={<Tables/> }/>
+              <Route path="/billing" element={<Billing/> }/>
+              <Route path="/profile" element={<Profile/> }/>
+          </Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/AppLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './layout/Main'
+
+
+const AppLayout = () => {
+    return (
+        <>
+            <Main>
+                
+            </Main>
+        </>
+    )
+}
+
+export default AppLayout;
