d29e692cbc6986a1a7fddcc81502d24e641df688
hongui <honge7694@naver.com>
2023-07-25 15:52:27 +0900

    FE Profile 최근 본 상품 Layout

---
+++ frontend/src/components/profile/RecentViewed.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Card, Row, Col, Typography, Carousel } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import Rating from 'react-rating-stars-component';
+
+import project1 from "../../assets/images/home-decor-1.jpeg";
+
+import { axiosInstance } from 'api';
+import { useAppContext } from 'store';
+
+
+const { Title, Text } = Typography;
+
+const RecentViewed = ({recentView}) => {
+    console.log('recentView : ', recentView);
+    const { store: token } = useAppContext();
+    const headers = { Authorization: `Bearer ${token['jwtToken']}`};
+    const history = useNavigate();
+    const carouselRef = useRef(null);
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+    };
+
+    const handlerOnClick = (e, id) => {
+        e.preventDefault();
+        console.log(id);
+
+        async function fetchRecentViewed() {
+            const data = { 
+                'product_id': id
+            }
+            try{
+                const response = await axiosInstance.post('/account/recent/viewed/', data, { headers });
+                console.log('ProductList RecentViewed response : ', response);
+            }catch(error){
+                console.log('error : ', error);
+            }
+        }
+        fetchRecentViewed();
+        
+        history('/product/'+id);
+    };
+    
+
+    const handlePrev = () => {
+        carouselRef.current.prev();
+    };
+
+    const handleNext = () => {
+        carouselRef.current.next();
+    };
+    
+
+    return (
+        <>
+        <Card
+            bordered={false}
+            className="header-solid mb-24"
+            title={
+            <>
+                <h6 className="font-semibold">최근 본 상품</h6>
+                <p>최근 하루동안 본 상품</p>
+            </>
+            }
+        >
+            <Carousel ref={carouselRef} {...settings}>
+                {recentView.map((recent) => (
+                    // <Col span={6} key={recent.id}>
+                        <a href="#" onClick={ (e) => handlerOnClick(e, recent.product.id) }>
+                            <Card
+                                hoverable
+                                cover={<img alt={recent.product.name} src={recent.product.images[0].image_src} style={{ height: '280px', objectFit: 'cover' }} />}
+                                style={{ display: 'grid', gridTemplateRows: 'auto 1fr auto', marginRight: '16px' }}
+                            >
+                                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                    <Title level={5}>{recent.product.name}</Title>
+                                    <Title level={5} style={{ marginBottom: 0 }}> ₩ {recent.product.price}</Title>
+                                </div>
+                                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                                    <Rating
+                                        count={5}
+                                        value={recent.product.rating}
+                                        size={24}
+                                        activeColor="#ffd700"
+                                        edit={false}
+                                    />
+                                    <Text type="secondary">{recent.product.view_count} hits</Text> 
+                                </div>
+                                <div>
+                                    <Text type="secondary">{recent.product.review_count} reviews</Text> 
+                                </div>
+                            </Card>
+                        </a>
+                    // </Col>
+                ))}
+            </Carousel>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                <LeftOutlined style={{ fontSize: '24px', cursor: 'pointer' }} onClick={handlePrev} />
+                <RightOutlined style={{ fontSize: '24px', cursor: 'pointer' }} onClick={handleNext} />
+            </div>
+        </Card>
+        </>
+    )
+}
+
+
+export default RecentViewed;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -14,23 +14,7 @@ import {
   message,
 } from "antd";
 
-import {
-  FacebookOutlined,
-  TwitterOutlined,
-  InstagramOutlined,
-  VerticalAlignTopOutlined,
-} from "@ant-design/icons";
-
 import BgProfile from "../assets/images/bg-profile.jpg";
-import profilavatar from "../assets/images/face-1.jpg";
-import convesionImg from "../assets/images/face-3.jpg";
-import convesionImg2 from "../assets/images/face-4.jpg";
-import convesionImg3 from "../assets/images/face-5.jpeg";
-import convesionImg4 from "../assets/images/face-6.jpeg";
-import convesionImg5 from "../assets/images/face-2.jpg";
-import project1 from "../assets/images/home-decor-1.jpeg";
-import project2 from "../assets/images/home-decor-2.jpeg";
-import project3 from "../assets/images/home-decor-3.jpeg";
 import { axiosInstance } from 'api';
 import { useRecoilValue, useResetRecoilState } from "recoil";
 import { useAppContext } from 'store';
@@ -38,76 +22,13 @@ import { userState } from 'state';
 import UserInfoEdit from 'components/profile/UserInfoEdit';
 import UserPasswordEdit from 'components/profile/UserPasswordEdit';
 import UserProfile from 'components/profile/UserProfile';
+import RecentViewed from 'components/profile/RecentViewed';
 
 function Profile() {
-  const [imageURL, setImageURL] = useState(false);
-  const [, setLoading] = useState(false);
-
-  const getBase64 = (img, callback) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => callback(reader.result));
-    reader.readAsDataURL(img);
-  };
-
-  const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-    if (!isJpgOrPng) {
-      message.error("You can only upload JPG/PNG file!");
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      message.error("Image must smaller than 2MB!");
-    }
-    return isJpgOrPng && isLt2M;
-  };
-
-  const handleChange = (info) => {
-    if (info.file.status === "uploading") {
-      setLoading(false);
-      return;
-    }
-    if (info.file.status === "done") {
-      getBase64(info.file.originFileObj, (imageUrl) => {
-        setLoading(false);
-        setImageURL(false);
-      });
-    }
-  };
-
-  const uploadButton = (
-    <div className="ant-upload-text font-semibold text-dark">
-      {<VerticalAlignTopOutlined style={{ width: 20, color: "#000" }} />}
-      <div>Upload New Project</div>
-    </div>
-  );
-
-
-  const project = [
-    {
-      img: project1,
-      titlesub: "Project #1",
-      title: "Modern",
-      disciption:
-        "As Uber works through a huge amount of internal management turmoil.",
-    },
-    {
-      img: project2,
-      titlesub: "Project #2",
-      title: "Scandinavian",
-      disciption:
-        "Music is something that every person has his or her own specific opinion about.",
-    },
-    {
-      img: project3,
-      titlesub: "Project #3",
-      title: "Minimalist",
-      disciption:
-        "Different people have different taste, and various types of music, Zimbali Resort",
-    },
-  ];
   const [selectedTab, setSelectedTab] = useState("a"); // Tab State
 
   const [userInfo, setUserInfo] = useState([]); // UserInfo State
+  const [recentView, setRecentView] = useState([]); // UserInfo State
   const user = useRecoilValue(userState);
   const user_id = user['userId'];
   const resetUser = useResetRecoilState(userState);
@@ -118,20 +39,34 @@ function Profile() {
   const headers = { Authorization: `Bearer ${token['jwtToken']}`};
   
 
-  // 유저 정보 불러오기.
   useEffect(() => {
+    // 유저 정보 불러오기.
     async function fetchUserInfo() {
       try{
         const { data } = await axiosInstance.get(apiUrl, {headers});
         console.log("user_data :", data);
         setUserInfo(data);
       }catch(error){
-        console.log('error : ', error);
+        console.log('user_data error : ', error);
         resetUser();
         history('/sign-in');
       }
     }
     fetchUserInfo();
+
+    // 최근 본 상품 불러오기
+    async function fetchRecentViewed() {
+      try{
+        const { data } = await axiosInstance.get('account/recent/viewed/', {headers});
+        console.log("recent_data :", data);
+        setRecentView(data);
+      }catch(error){
+        console.log('recent_data error : ', error);
+        resetUser();
+        history('/sign-in');
+      }
+    }
+    fetchRecentViewed();
   }, []);
 
   const handleTabChange = (e) => {
@@ -186,62 +121,7 @@ function Profile() {
       {selectedTab === "c" && <UserPasswordEdit data={{userInfo}}/>}
 
 
-      <Card
-        bordered={false}
-        className="header-solid mb-24"
-        title={
-          <>
-            <h6 className="font-semibold">최근 본 상품</h6>
-            <p>Architects design houses</p>
-          </>
-        }
-      >
-        <Row gutter={[24, 24]}>
-          {project.map((p, index) => (
-            <Col span={24} md={12} xl={6} key={index}>
-              <Card
-                bordered={false}
-                className="card-project"
-                cover={<img alt="example" src={p.img} />}
-              >
-                <div className="card-tag">{p.titlesub}</div>
-                <h5>{p.titile}</h5>
-                <p>{p.disciption}</p>
-                <Row gutter={[6, 0]} className="card-footer">
-                  <Col span={12}>
-                    <Button type="button">VIEW PROJECT</Button>
-                  </Col>
-                  <Col span={12} className="text-right">
-                    <Avatar.Group className="avatar-chips">
-                      <Avatar size="small" src={profilavatar} />
-                      <Avatar size="small" src={convesionImg} />
-                      <Avatar size="small" src={convesionImg2} />
-                      <Avatar size="small" src={convesionImg3} />
-                    </Avatar.Group>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-          ))}
-          <Col span={24} md={12} xl={6}>
-            <Upload
-              name="avatar"
-              listType="picture-card"
-              className="avatar-uploader projects-uploader"
-              showUploadList={false}
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              beforeUpload={beforeUpload}
-              onChange={handleChange}
-            >
-              {imageURL ? (
-                <img src={imageURL} alt="avatar" style={{ width: "100%" }} />
-              ) : (
-                uploadButton
-              )}
-            </Upload>
-          </Col>
-        </Row>
-      </Card>
+      {recentView && <RecentViewed recentView={recentView} />}
     </>
   );
 }
